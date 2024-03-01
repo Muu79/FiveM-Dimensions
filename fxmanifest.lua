6bfb8eb5e95a0004013638022c53d5f8154fe91e
Muaaz Bhyat <muu794@gmail.com>
2024-03-01 15:08:03 +0200

    Added initial files

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+
+
+-- Client Scripts
+
+client_scripts {
+    'client/client.lua'
+}
+
+
+-- Server Scripts
+
+server_scripts{
+    'server/server.lua'
+}
